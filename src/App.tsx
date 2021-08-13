9e2a63fa929a3d493420924288cfd69bb3926eed
Francisco Cobas <fcarocena@gmail.com>
2021-08-13 18:15:55 -0300

    Initial commit 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { useForm } from 'react-hook-form';
+
+type Time = Date;
 
 function App() {
+  const { register, handleSubmit } = useForm();
+  const [time, setTime] = useState<Time>(new Date(0, 0, 0));
+
+  const onSubmit = ({
+    hours,
+    minutes,
+    seconds,
+  }: {
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }) => {
+    setTime(new Date(2021, 8, 5, hours, minutes, seconds));
+  };
+
+  const handleStartClick = () => {
+    setInterval(() => {
+      setTime((time) => {
+        let s = time.getSeconds();
+        console.log(s);
+        time.setSeconds(s--);
+        console.log(time);
+        return time;
+      });
+    }, 1000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type='number' {...register('hours')} />
+        <input type='number' {...register('minutes')} />
+        <input type='number' {...register('seconds')} />
+        <button>Set</button>
+      </form>
+      <button onClick={() => handleStartClick()}>Start</button>
+      <div className='timer-container'>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
